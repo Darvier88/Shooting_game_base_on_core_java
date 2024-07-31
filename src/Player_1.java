102bc42771e865d7190a2fcced3c06e8eca7f43e
Darvier88 <127569059+Darvier88@users.noreply.github.com>
2024-07-31 02:35:19 -0500

    Update Player_1.java

--- src/Player_1.java
+++ src/Player_1.java
@@ -1,4 +1,4 @@
-public class Player_1 {
+public class Player_1 extends Player {
     private String name;
     private String weapon;
     private int health;
@@ -15,53 +15,39 @@ public class Player_1 {
 
     }
 
-    public String getName() {
-        return name;
-    }
+    // Implementación de métodos de la interfaz Player
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getWeapon() { return weapon; }
+    public void setWeapon(String weapon) { this.weapon = weapon; }
+    public int getHealth() { return health; }
+    public void setHealth(int health) { this.health = health; }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getWeapon() {
-        return weapon;
-    }
-
-    public void setWeapon(String weapon) {
-        this.weapon = weapon;
-    }
-
-    public int getHealth() {
-        return health;
-    }
-
-    public void setHealth(int health) {
-        this.health = health;
-    }
-
-    public void Hit_By_Gun1(){
-        this.health-=20;
-        if(this.health<=0){
-            this.health=0;
+    public void hitByGun1() {
+        this.health -= 20;
+        if(this.health <= 0) {
+            this.health = 0;
         }
-        System.out.println("Hit by gun 1 health reduced by 20 and new health is :"+this.health);
-        if (this.health==0){
-            System.out.println(getName()+" is dead");
+        System.out.println("Hit by gun 1 health reduced by 20 and new health is :" + this.health);
+        if (this.health == 0) {
+            System.out.println(getName() + " is dead");
         }
     }
-    public void Hit_By_Gun2(){
-       this.health-=30;
-       if (this.health<=0){
-           this.health=0;
-       }
-        System.out.println("Hit by gun 2 health reduced by 30 and new health is :"+this.health);
-       if (this.health==0){
-           System.out.println(getName()+ " is dead");
-       }
+
+    public void hitByGun2() {
+        this.health -= 30;
+        if (this.health <= 0) {
+            this.health = 0;
+        }
+        System.out.println("Hit by gun 2 health reduced by 30 and new health is :" + this.health);
+        if (this.health == 0) {
+            System.out.println(getName() + " is dead");
+        }
     }
-    public void Head_shoot(){
-       this.health-=health;
-        System.out.println("Head shoot your health is: "+this.health+" "  +getName()+" is dead");
+
+    public void headshot() {
+        this.health = 0;
+        System.out.println("Head shoot your health is: " + this.health + " "  + getName() + " is dead");
     }
 
 }
