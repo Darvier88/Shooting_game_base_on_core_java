74f8d3cdbd8afdc712a0982a62e14f95199811c8
Darvier88 <127569059+Darvier88@users.noreply.github.com>
2024-07-31 02:34:29 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        Player_1 Abhishek = new Player_1("Jhon","AK-47",100);
-//        Abhishek.Hit_By_Gun1();
-//        Abhishek.Hit_By_Gun1();
-//        Abhishek.Head_shoot();
-//        System.out.println(Abhishek.getHealth());
-//        Abhishek.Hit_By_Gun2();
-//        Abhishek.Hit_By_Gun2();
-//        Abhishek.Hit_By_Gun2();
-//        Abhishek.Hit_By_Gun2();
-        Player_2 Ashu = new Player_2("Tom","Machine gun",100,true);
-//        Ashu.Hit_By_Gun1();
-//        Ashu.Hit_By_Gun1();
-        Ashu.Hit_By_Gun2();
-        Ashu.Hit_By_Gun2();
-        Ashu.Hit_By_Gun2();
-        Ashu.Hit_By_Gun2();
-        Ashu.Hit_By_Gun2();
-        Ashu.Hit_By_Gun2();
+        PlayerFactory player1Factory = new Player1Factory();
+        PlayerFactory player2Factory = new Player2Factory();
 
+        Player player1 = player1Factory.createPlayer("Jhon", "AK-47", 100, null);
+        Player player2 = player2Factory.createPlayer("Tom", "Machine gun", 100, true);
 
-
-
-
+        player2.hitByGun2();
+        player2.hitByGun2();
+        player2.hitByGun2();
+        player2.hitByGun2();
+        player2.hitByGun2();
+        player2.hitByGun2();
     }
 }
