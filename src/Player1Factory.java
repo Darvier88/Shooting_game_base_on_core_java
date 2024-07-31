a225b2e07036347297caa211e1fa592487b86154
Darvier88 <127569059+Darvier88@users.noreply.github.com>
2024-07-31 03:25:44 -0500

    Delete src/Player1Factory.java

--- src/Player1Factory.java
+++
@@ -1,7 +0,0 @@
-
-public class Player1Factory extends PlayerFactory {
-	 @Override
-	 public Player createPlayer(String name, String weapon, int health, Boolean armour) {
-		 return new Player_1(name, weapon, health);
-	 }
-}
